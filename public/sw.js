e1b018a1f4e01570985503e87a8c241f5cc38a66
OBUSSIN <osazeeobaseki69@gmail.com>
2026-03-23 20:46:00 -0500

    Refactor fetch event listener in service worker

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,19 @@
-importScripts("/scram/scramjet.all.js");
+// 1. Import the BareMux engine so the SW can actually connect to the internet
+importScripts("/baremux/bare.cjs");
+// 2. Import the Scramjet worker (NOT all.js)
+importScripts("/scram/scramjet.worker.js");
 
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
 
 async function handleRequest(event) {
-	await scramjet.loadConfig();
-	if (scramjet.route(event)) {
-		return scramjet.fetch(event);
-	}
-	return fetch(event.request);
+  await scramjet.loadConfig();
+  if (scramjet.route(event)) {
+    return scramjet.fetch(event);
+  }
+  return fetch(event.request);
 }
 
 self.addEventListener("fetch", (event) => {
-	event.respondWith(handleRequest(event));
+  event.respondWith(handleRequest(event));
 });
